ebc883f05dc096b37ba2b094709ee5802e7b4860
eons <eons@eons.dev>
2022-10-24 19:28:31 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# api_operation
-An APIE Endpoint for doing work with a resource.
+# APIE Operation Endpoint
+
+APIE Operations provide a consistent means of interacting with any resource in any way. This is a generalized system without many restrictions.
+
+For more information on how operations can be used with apie, see [the apie Resource Paradigm](https://github.com/eons-dev/bin_apie/#resource-paradigm).
+
+## What You Need to Know
+
+There's only really one key setting for an operation: its name.  
+The name of the operation determines what method is called when its `Call()`ed. The required method is "{name}_implementation". For example, the "list" operation would call "list_implementation()".
+
+You are also provided `PreOperation()` and `PostOperation()` hooks, should you need them. 
+
+All other settings can be found in [apie.Endpoint](https://github.com/eons-dev/bin_apie/#api-endpoints) and [eons.Functor](https://github.com/eons-dev/lib_eons/#functors).
---
+++ inc/api_operation.py
@@ -0,0 +1,43 @@
+import logging
+import apie
+
+class operation(apie.Endpoint):
+    def __init__(this, name="API Endpoint for any Operation"):
+        super().__init__(name)
+
+        this.requiredMethods.append(f"{this.name}_implementation")
+
+        # Everything that can change an operation should be specified in the request.
+        this.fetchFrom = [
+            'this',
+            'args',
+            'request_args',
+            'request_form',
+            'request_json',
+            'request_files',
+        ]
+
+    # Required Endpoint method. See that class for details.
+    def GetHelpText(this):
+        return f'''\
+------------------------------------
+Implemented by {this.precursor.name};
+(reminder: you cannot change any variables referenced beyond this point)
+
+{this.precursor.GetHelpText()}
+'''
+
+    # Anything you'd like to do before the operation
+    def PreOperation(this):
+        pass
+
+    # Anything you'd like to do after the operation
+    def PostOperation(this):
+        pass
+
+    def Call(this):
+        this.PreOperation()
+        if (this.precursor and hasattr(this.precursor, 'implemented')):
+            this.precursor.implemented = this
+        eval(f"this.{this.name}_implementation()")
+        this.PostOperation()
